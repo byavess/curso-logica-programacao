bf98e3d3809295425ec3960c48b273110a352622
byavess <byavess@gmail.com>
2022-04-17 19:48:37 -0300

    categoria indefinida

--- src/NomeIdadeParticipantes.java
+++ src/NomeIdadeParticipantes.java
@@ -1,7 +1,7 @@
 public class NomeIdadeParticipantes {
     public static void main(String[] args) {
 
-       String nome = " Arthur";
+        String nome = " Arthur";
         int idade = 12;
         if (idade <= 10) {
             System.out.println(nome + "Categoria Infantil" + idade);
@@ -12,6 +12,8 @@ public class NomeIdadeParticipantes {
             System.out.println(nome + "Categoria Pré Adulto" + idade);
         } else if (idade >= 20) {
             System.out.println(nome + "Categoria Adulto" + idade);
+        } else {
+            System.out.println(nome + "Categoria indefinida + idade");
         }
     }
 }
